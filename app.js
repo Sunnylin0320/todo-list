14f7d5c1a0ae3e540d1eec28dcaaa903b1f25835
Sunnylin0320 <sunny8503201@gmail.com>
2023-05-10 07:20:13 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -43,8 +43,12 @@ app.get("/", (req, res) => {
     .catch((error) => console.error(error)); // 錯誤處理
 });
 
-app.get("/todos/new", (req, res) => {
-  return res.render("new");
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render("detail", { todo }))
+    .catch((error) => console.log(error));
 });
 
 app.post("/todos", (req, res) => {
