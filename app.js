37186daecc6295f45b71b29a4d530360e0b17f8e
Sunnylin0320 <sunny8503201@gmail.com>
2023-05-10 06:52:03 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require("express-handlebars");
+const Todo = require('./models/todo') // 載入 Todo model
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -31,8 +32,12 @@ app.set("view engine", "hbs");
 
 // 設定首頁路由
 app.get("/", (req, res) => {
-  res.render("index");
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)); // 錯誤處理
 });
+
 // 設定 port 3000
 app.listen(3000, () => {
   console.log("App is running on http://localhost:3000");
