4253863e7b1a95c2238786446102e8717b9a4b37
Sunnylin0320 <sunny8503201@gmail.com>
2023-05-13 13:19:47 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
-const mongoose = require('mongoose') // 載入 mongoose
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -15,24 +14,9 @@ const methodOverride = require('method-override')
 
 // 引用路由器
 const routes = require('./routes')
-
-
+require("./config/mongoose");
 
 const app = express();
-
-
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
