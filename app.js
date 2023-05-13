0b6d91dd58eb6b45a5d1672e5a5462413038dbca
Sunnylin0320 <sunny8503201@gmail.com>
2023-05-13 13:00:30 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ const bodyParser = require("body-parser");
 // 載入 method-override
 const methodOverride = require('method-override') 
 
-const Todo = require("./models/todo");
+
+// 引用路由器
+const routes = require('./routes')
+
+
+
 const app = express();
 
 
@@ -36,61 +41,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
-// 設定首頁路由
-app.get("/", (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: "asc" }) // 新增這裡：根據 _id 升冪排序
-    .then((todos) => res.render("index", { todos }))
-    .catch((error) => console.error(error));
-});
+// 將 request 導入路由器
+app.use(routes)
 
-
-app.get("/todos/new", (req, res) => {
-  return res.render("new");
-});
-app.post("/todos", (req, res) => {
-  const name = req.body.name;
-  return Todo.create({ name })
-    .then(() => res.redirect("/"))
-    .catch((error) => console.log(error));
-});
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("detail", { todo }))
-    .catch((error) => console.log(error));
-});
-app.get("/todos/:id/edit", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("edit", { todo }))
-    .catch((error) => console.log(error));
-});
-
-
-app.put("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  const { name, isDone } = req.body;
-  return Todo.findById(id)
-    .then((todo) => {
-      todo.name = name;
-      todo.isDone = isDone === "on";
-      return todo.save();
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch((error) => console.log(error));
-});
-
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .then((todo) => todo.remove())
-    .then(() => res.redirect("/"))
-    .catch((error) => console.log(error));
-});
 
 
 // 設定 port 3000
